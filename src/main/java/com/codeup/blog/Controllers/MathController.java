6dd0dafe8a620db33ff7702e9bb91b34fed1c8d9
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-06-22 09:56:47 -0500

    number 2

--- src/main/java/com/codeup/blog/Controllers/MathController.java
+++ src/main/java/com/codeup/blog/Controllers/MathController.java
@@ -8,8 +8,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MathController {
     @GetMapping("/add/3/and/4")
     @ResponseBody
-    public String index(){
+    public String add(){
         return "7";
+    }
+
+    @GetMapping("/subtract/3/from/10")
+    @ResponseBody
+    public String subtract(){
+        return "7";
+    }
+
+    @GetMapping("/multiply/4/and/5")
+    @ResponseBody
+    public String multiply(){
+        return "20";
+    }
+
+    @GetMapping("/divide/6/by/3")
+    @ResponseBody
+    public String divide(){
+        return "2";
     }
 
 
