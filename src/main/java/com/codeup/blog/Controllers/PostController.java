d41dd50e574e3be4c36e25596ea1c831cf916a96
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-06-30 15:07:09 -0500

    email notification sends when a post is created. NOTE: CHANGE PORT IF IT GETS TIMEDOUT

--- src/main/java/com/codeup/blog/Controllers/PostController.java
+++ src/main/java/com/codeup/blog/Controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.codeup.blog.Controllers;
 
+import com.codeup.blog.Services.EmailService;
 import com.codeup.blog.daos.PostsRepository;
 import com.codeup.blog.daos.UsersRepository;
 import com.codeup.blog.models.Post;
@@ -15,12 +16,13 @@ public class PostController {
 
     private PostsRepository postsDao;
     private UsersRepository usersDao;
+    private final EmailService emailService;
 
-    public PostController(PostsRepository postsRepository, UsersRepository usersRepository){
-        postsDao = postsRepository;
-        usersDao = usersRepository;
+    public PostController(PostsRepository postsRepository, UsersRepository usersRepository, EmailService emailService){
+        this.postsDao = postsRepository;
+        this.usersDao = usersRepository;
+        this.emailService = emailService;
     }
-
 //    @GetMapping("/posts") another way doGet is on the line below Do not do both getmapping and requestmapping
 //    @RequestMapping(value = "/posts", method = RequestMethod.GET)
     @GetMapping("/posts")
@@ -51,6 +53,8 @@ public class PostController {
         User currentUser = usersDao.getOne(1L);
         postToBeSaved.setOwner(currentUser);
         Post savedPost= postsDao.save(postToBeSaved);
+        emailService.prepareAndSend(savedPost, "A new ad has been creating", "An ad has been created with the id of " + savedPost.getId());
+        emailService.prepareAndSend(savedPost,"A new post has been posted", "A Post has been created with an id of: " + savedPost.getId());
         return "redirect:/posts/" +savedPost.getId();
     }
 
